bdc9563538b18b288948a99609d38cfa684fff19
MalaySaxena <dynamic.malay@gmail.com>
2020-12-14 12:57:42 +0530

    First Commit

---
+++ src/main/java/com/dbms/Residence/Finder/MetroResidenceFinderApplication.java
@@ -0,0 +1,13 @@
+package com.dbms.Residence.Finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroResidenceFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroResidenceFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbms/Residence/Finder/MetroResidenceFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbms.Residence.Finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroResidenceFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
