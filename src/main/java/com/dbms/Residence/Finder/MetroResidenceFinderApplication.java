ac5ea98ef60c079915f72d5d4765b4b33c844f82
Lavish Garg <53224426+Lavish171@users.noreply.github.com>
2020-12-14 16:53:47 +0530

    commenting sout

--- src/main/java/com/dbms/Residence/Finder/MetroResidenceFinderApplication.java
+++ src/main/java/com/dbms/Residence/Finder/MetroResidenceFinderApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MetroResidenceFinderApplication {
 
 	public static void main(String[] args) {
+		System.out.println("My name is lavish");
 		SpringApplication.run(MetroResidenceFinderApplication.class, args);
 	}
 
