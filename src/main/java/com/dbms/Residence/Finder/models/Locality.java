a3e9094355ca50e48bb09174110272bb429fa1cf
MalaySaxena <dynamic.malay@gmail.com>
2020-12-14 17:29:11 +0530

    Create Locality.java

---
+++ src/main/java/com/dbms/Residence/Finder/models/Locality.java
@@ -0,0 +1,79 @@
+package com.dbms.Residence.Finder.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Locality {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String longitude;
+
+    private String latitude;
+
+    private String address;
+
+    private String city;
+
+    private String state;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Locality(String longitude, String latitude, String address, String city, String state) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+    }
+}
