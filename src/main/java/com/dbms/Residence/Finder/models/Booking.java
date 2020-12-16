098cbcf49bf562ad8ac3c9b3927eb5c600310f2d
MalaySaxena <dynamic.malay@gmail.com>
2020-12-16 16:05:03 +0530

    Update Booking.java

--- src/main/java/com/dbms/Residence/Finder/models/Booking.java
+++ src/main/java/com/dbms/Residence/Finder/models/Booking.java
@@ -1,5 +1,7 @@
 package com.dbms.Residence.Finder.models;
 
+import com.dbms.Residence.Finder.models.PrimaryKeys.BookingPK;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.sql.Date;
@@ -8,45 +10,6 @@ import java.util.Objects;
 
 @Entity
 public class Booking {
-
-    @Embeddable
-    static class BookingPK implements Serializable{
-
-        private Long tenantId;
-
-        private Long propertyId;
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            BookingPK bookPk = (BookingPK) o;
-            return tenantId == bookPk.tenantId &&
-                    propertyId == bookPk.propertyId;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(tenantId, propertyId);
-        }
-
-        public Long getTenantId() {
-            return tenantId;
-        }
-
-        public void setTenantId(Long tenantId) {
-            this.tenantId = tenantId;
-        }
-
-        public Long getPropertyId() {
-            return propertyId;
-        }
-
-        public void setPropertyId(Long propertyId) {
-            this.propertyId = propertyId;
-        }
-    }
-
 
     @EmbeddedId
     private BookingPK id;
